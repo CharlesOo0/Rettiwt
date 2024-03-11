1a1f2dcc6ea34ac67fcc346ddad623792e7a699e
Charles FERNANDEZ <charles.fernandez@utbm.fr>
2024-03-11 16:43:17 +0100

    Le login marche.

--- login_register.php
+++ login_register.php
@@ -7,9 +7,52 @@
 </head>
 
 <body>
+
+    <?php
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") { // Rajoute un event listener a la page pour attendre un POST request
+        $name = $email = "";
+        $name = htmlspecialchars($_POST['username']);
+        $password = htmlspecialchars($_POST['password']);
+
+
+        // -------------------------- Connexion à la base de données -------------------------- //
+        $serveur = "localhost"; // Adresse du serveur MySQL (généralement localhost)
+        $utilisateur = "root"; // Nom d'utilisateur MySQL
+        $motdepasse = ""; // Mot de passe MySQL
+        $basededonnees = "rettiwt"; // Nom de la base de données
+
+        $connexion = mysqli_connect($serveur, $utilisateur, $motdepasse, $basededonnees);
+
+        // Vérifie la connextion
+        if (!$connexion) {
+            die("La connexion à la base de données a échoué : " . mysqli_connect_error());
+        }
+
+        // SQL query to select data
+        $sql = "SELECT * FROM profil WHERE username='$name' AND password='$password'";
+
+        // Execute the query
+        $result = mysqli_query($connexion, $sql);
+
+        if ($result->num_rows == 1) {
+            echo "<p> Login succesful redirecting ... </p>";
+            header("Location: http://localhost/WE4A_projet/home.php");
+        } else {
+            echo "<p> Wrong password or email </p>";
+        }
+
+    }
+
+
+
+
+
+    ?>
+
     <h1>Login page :</h1>
 
-    <form action="POST">
+    <form method="POST" action="http://localhost/WE4A_projet/login_register.php">
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" required><br><br>
 
