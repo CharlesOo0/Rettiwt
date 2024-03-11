3941b46aa034e09231a71a61fdd11d2e5a0f924f
Charles FERNANDEZ <charles.fernandez@utbm.fr>
2024-03-11 16:12:45 +0100

    Les toutes bases

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 <html>
 
 <head>
-    <title>Home</title>
+    <title>Rettiwt</title>
 </head>
 
 <body>
---
+++ login_register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Rettiwt</title>
+</head>
+
+<body>
+    <h1>Login page :</h1>
+
+    <form action="POST">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br><br>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <input type="submit" value="Login">
+    </form>
+</body>
+
+</html>
