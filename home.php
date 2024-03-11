817b94703c9d48cd2b67895f8ee603d334a4025e
Charles FERNANDEZ <charles.fernandez@utbm.fr>
2024-03-11 16:08:02 +0100

    First commit

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Home</title>
+</head>
+
+<body>
+    <h1>Welcome to the home page</h1>
+    <p>Click <a href="logout.php">here</a> to logout.</p>
+</body>
+
+</html>
