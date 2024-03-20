dbd8a69365dc0ae3cb37fb0b05b23abe266ae1b6
Charles <didi831313@gmail.com>
2024-03-20 09:40:25 +0100

    Ca va retout changer pour du bootstrap

--- test.php
+++ test.php
@@ -2,36 +2,44 @@
 <html>
 <head>
     <title>My Test Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/freestyle.css">
 </head>
 <body>
+    <div class="container-fluid">
 
-    <div id="left-band">
-        Bandeau gauche
-    </div>
-    
-    <div id="post" >
-        Les posts
-    </div>
+    <div class="row">
 
-    <div id="right-band">
-
-        <div id="profil">
-            Profil
-
-            <div id="profil-detail">
-                <div id="avatar">Avatar</div>
-                <div id="pseudo-follow">pseudo et follower</div>
-            </div>
-
-            <div id="bio">
-                Bio
-            </div>
+        <div id="left-band" class="col-md-3">
+            Bandeau gauche
+        </div>
+        
+        <div id="post" class="col-md-5">
+            Les posts
         </div>
 
-        <div id="post-form">
-            Post form
+        <div id="right-band" class="col-md-4">
+
+            <div id="profil" class="container-fluid">
+                Profil
+
+                <div id="profil-detail" class="row">
+                    <div id="avatar" class="col">Avatar</div>
+                    <div id="pseudo-follow" class="col">pseudo et follower</div>
+                </div>
+
+                <div id="bio" class="row">
+                    Bio : Je m'appelle Jean et j'aime les chats et les jeux vidéos ! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </div>
+
+                <div id="post-form" class="row">
+                    Post form
+                </div>
+            </div>
+
         </div>
+
+    </div>
 
     </div>
     
